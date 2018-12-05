80a0b8b02d3ecc60813a73da1d12c7c5ee316257
greedyshardul <aeershardul@gmail.com>
2018-12-05 18:46:11 +0530

    firebase and map collision solved

--- android/app/src/main/java/com/blindproj/MainApplication.java
+++ android/app/src/main/java/com/blindproj/MainApplication.java
@@ -1,7 +1,7 @@
 package com.com.blindproj;
 
 import android.app.Application;
-
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -26,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RNFirebaseAuthPackage()
+            new RNFirebaseAuthPackage(),
+            new MapsPackage()
       );
     }
 
